cfac4b6c96ec6d82cb8babaa4509e10fcc4eb1a4
Dan Pelensky <dan@pelensky.com>
2017-08-15 13:53:36 +0100

    added credentials

--- src/tic_tac_toe/queue.clj
+++ src/tic_tac_toe/queue.clj
@@ -1,6 +1,13 @@
 (ns tic-tac-toe.queue
   (require [amazonica.aws.sqs :as aws]))
 
+(defn- get-environment-variable [name]
+  (str (get (System/getenv) name)))
+
+(def cred {:access-key (get-environment-variable "AWS_ACCESS_KEY_ID")
+           :secret-key (get-environment-variable "AWS_SECRET_KEY")
+           :endpoint (get-environment-variable "AWS_REGION_ENV_VAR")})
+
 (def queue (aws/find-queue "InProgressGames"))
 
 (defn create-uuid []
@@ -10,7 +17,8 @@
   (aws/send-message queue uuid))
 
 (defn get-game-uuids []
-  (aws/receive-message :queue-url queue
+  (aws/receive-message 
+                     :queue-url queue
                        :wait-time-seconds 6
                        :max-number-of-messages 10
                        :delete false
