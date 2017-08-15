73f37db5057cef2809c292cd0ba421a05e6f6531
Dan Pelensky <dan@pelensky.com>
2017-08-15 15:52:59 +0100

    refactored queue namespace

---
+++ spec/tic_tac_toe/queue_spec.clj
@@ -0,0 +1,13 @@
+(ns tic-tac-toe.queue-spec
+  (require [speclj.core :refer :all]
+           [tic-tac-toe.queue :refer :all]))
+
+(describe "Queue"
+
+          (it "creates a uuid"
+              (should= 36
+                       (count (create-uuid))))
+
+          (it "gets the game ids from the message queue"
+              (should= #{"test1" "test2" "test3"}
+                       (get-game-ids {:messages [{:body "test1"} {:body "test2"} {:body "test3"}]}))))
--- src/tic_tac_toe/queue.clj
+++ src/tic_tac_toe/queue.clj
@@ -1,9 +1,6 @@
 (ns tic-tac-toe.queue
   (require [amazonica.aws.sqs :as aws]))
 
-(defn- get-environment-variable [name]
-  (str (get (System/getenv) name)))
-
 (def queue (aws/find-queue "InProgressGames"))
 
 (defn create-uuid []
@@ -19,5 +16,6 @@
                        :delete false
                        :attribute-names ["All"]))
 
-(defn get-game-ids []
-  (map #(get % :body) (get (tic-tac-toe.queue/get-messages) :messages)))
+(defn get-game-ids [all-messages]
+  (let [messages (get all-messages :messages)]
+  (set (map #(get % :body) messages))))
